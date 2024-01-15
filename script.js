410bc73b0d5a88183df7dd477d208269e010fc7d
wadiah <wadiah.tm@gmail.com>
2024-01-15 15:12:36 -0500

    banner edit

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-let text = "testing testing testing maybe i'll put the weather forecast here";
-let result = text.repeat(1);
+// let link = document.createElement("a");
+// let text = document.createTextNode("Currently looking for work! Find me on ");
 
 
-document.getElementById("banner").innerHTML = result;
-document.getElementById("banner").animate(
-  [
-    // keyframes
-    { transform: "translateX(100%)" },
-    { transform: "translateX(-100%)" },
-  ],
-  {
-    // timing options
-    duration: 30000,
-    iterations: Infinity,
-  }
-);
+// link.appendChild(text);
+// link.title = "LinkedIn";
+// link.href = "https://linkedin.com";
+// let el = document.getElementById("banner");
+// el.appendChild(link);
+
+// let result = text.repeat(1);
 
 
-
+// document.getElementById("banner").innerHTML = result;
+// document.getElementById("banner").animate(
+//   [
+//     // keyframes
+//     { transform: "translateX(100%)" },
+//     { transform: "translateX(-100%)" },
+//   ],
+//   {
+//     // timing options
+//     duration: 30000,
+//     iterations: Infinity,
+//   }
+// );
